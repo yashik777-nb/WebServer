bc6829f641057dc9537769b4c666d96936de38a3
Yash <yaswanth.iddibani@gmail.com>
2021-01-17 12:05:44 +0000

    Express with HTML

--- src/app.js
+++ src/app.js
@@ -1,25 +1,32 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+
+// Serve the HTML Content folder
+const publicDirectoryFolder = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryFolder));
 
 // Domain = app.com
 // pages -> app.com/about
 // pages -> app.com/help
 
-app.get("", (req, res) => {
-  res.send("Express Tutorial");
-});
+// app.get("", (req, res) => {
+//   res.send(`<h1>Weather</h1>`);
+// });
 
-app.get("/help", (req, res) => {
-  res.send("Help Page Details");
-});
+// app.get("/help", (req, res) => {
+//   res.send([{ name: "Yash" }, { name: "Sai" }]);
+// });
 
-app.get("/about", (req, res) => {
-  res.send("Page Title");
-});
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About Title</h1>");
+// });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather Title");
+  res.send({
+    weather: "Hyderabad ",
+  });
 });
 
 // Start the server
