a38788bb8df0bb50d31943eb473ea7045beb8282
Yash <yaswanth.iddibani@gmail.com>
2021-01-17 13:54:31 +0000

    Handle Bars

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ const app = express();
 
 // Serve the HTML Content folder
 const publicDirectoryFolder = path.join(__dirname, "../public");
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryFolder));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Yash",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Yash",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "Help Yourself",
+  });
+});
 
 // Domain = app.com
 // pages -> app.com/about
